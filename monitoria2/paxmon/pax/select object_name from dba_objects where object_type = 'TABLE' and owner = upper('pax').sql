b8741c2497fe3e9c03e8ab9a0eb4a9182d133bfd
andre-franco <andre.franco@paxtecnologia.com.br>
2016-08-17 05:58:25 -0300

    Wed Aug 17 05:58:25 BRT 2016

--- monitoria2/paxmon/pax/select object_name from dba_objects where object_type = 'TABLE' and owner = upper('pax').sql
+++
@@ -1,6 +0,0 @@
-select DBMS_METADATA.GET_DDL('TABLE','select object_name from dba_objects where object_type = 'TABLE' and owner = upper('pax')') from DUAL
-                                                                                               *
-ERROR at line 1:
-ORA-00907: missing right parenthesis
-
-
