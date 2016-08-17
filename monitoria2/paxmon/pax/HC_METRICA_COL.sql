1a4ce840e78c4522fbdc5f63e30c0a183a406618
andre-franco <andre.franco@paxtecnologia.com.br>
2016-08-17 04:06:52 -0300

    Wed Aug 17 04:06:52 BRT 2016

---
+++ monitoria2/paxmon/pax/HC_METRICA_COL.sql
@@ -0,0 +1,7 @@
+ERROR:
+ORA-31603: object "HC_METRICA_COL" of type TABLE not found in schema "PAX"
+ORA-06512: at "SYS.DBMS_METADATA", line 6069
+ORA-06512: at "SYS.DBMS_METADATA", line 8666
+ORA-06512: at line 1
+
+
