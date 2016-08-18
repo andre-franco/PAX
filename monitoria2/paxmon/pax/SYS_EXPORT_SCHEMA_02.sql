cfa6ffe559de1c975edba4684569d5df66c1de4f
andre-franco <andre.franco@paxtecnologia.com.br>
2016-08-17 23:06:38 -0300

    Wed Aug 17 23:06:38 BRT 2016

--- monitoria2/paxmon/pax/SYS_EXPORT_SCHEMA_02.sql
+++
@@ -1,7 +0,0 @@
-ERROR:
-ORA-31603: object "SYS_EXPORT_SCHEMA_02" of type TABLE not found in schema "GIT"
-ORA-06512: at "SYS.DBMS_METADATA", line 6069
-ORA-06512: at "SYS.DBMS_METADATA", line 8666
-ORA-06512: at line 1
-
-
